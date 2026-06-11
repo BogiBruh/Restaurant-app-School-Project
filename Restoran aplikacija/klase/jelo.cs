28da3765d025145dba7a7f212330498a94fba076
Bogi <57872738+BogiBruh@users.noreply.github.com>
2026-06-11 14:33:43 +0200

    moved jelo.cs, modified databaza.cs
    moved the jelo class, added a function in databaza class to read out the jelo table and fill out a List<jelo> object

---
+++ Restoran aplikacija/klase/jelo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restoran_aplikacija
+{
+    class jelo
+    {
+        private int idJela;
+        private string naziv;
+        private int cena;
+
+        public jelo(int idJela, string naziv, int cena)
+        {
+            this.IdJela = idJela;
+            this.Naziv = naziv;
+            this.Cena = cena;
+        }
+
+        public jelo()
+        {
+            //nista, sorry
+        }
+
+        public int IdJela { get => idJela; set => idJela = value; }
+        public string Naziv { get => naziv; set => naziv = value; }
+        public int Cena { get => cena; set => cena = value; }
+    }
+}
