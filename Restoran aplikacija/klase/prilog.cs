d491e3c38897ac2b8f2cc9af6823dcf9abae3b43
Bogi <57872738+BogiBruh@users.noreply.github.com>
2026-06-11 14:22:20 +0200

    added prilog.cs

---
+++ Restoran aplikacija/klase/prilog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restoran_aplikacija.klase
+{
+    class prilog
+    {
+        private int idPriloga;
+        private int cenaPriloga;
+        private string nazivPriloga;
+
+        public prilog(int idPriloga, int cenaPriloga, string nazivPriloga)
+        {
+            this.IdPriloga = idPriloga;
+            this.CenaPriloga = cenaPriloga;
+            this.NazivPriloga = nazivPriloga;
+        }
+
+        public int IdPriloga { get => idPriloga; set => idPriloga = value; }
+        public int CenaPriloga { get => cenaPriloga; set => cenaPriloga = value; }
+        public string NazivPriloga { get => nazivPriloga; set => nazivPriloga = value; }
+    }
+}
