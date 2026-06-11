75c75166871b266e4b4d788d5b4496a7ddcf87a2
Bogi <57872738+BogiBruh@users.noreply.github.com>
2026-06-11 14:30:58 +0200

    Added racun classes

---
+++ Restoran aplikacija/klase/stavkaRacuna.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restoran_aplikacija.klase
+{
+    class stavkaRacuna
+    {
+        private int idRacuna;
+        private int idJela;
+        private int idPriloga;
+        private int cenaJela;
+        private int cenaPriloga;
+
+        public stavkaRacuna(int idRacuna, int idJela, int idPriloga, int cenaJela, int cenaPriloga)
+        {
+            this.IdRacuna = idRacuna;
+            this.IdJela = idJela;
+            this.IdPriloga = idPriloga;
+            this.CenaJela = cenaJela;
+            this.CenaPriloga = cenaPriloga;
+        }
+
+        public int IdRacuna { get => idRacuna; set => idRacuna = value; }
+        public int IdJela { get => idJela; set => idJela = value; }
+        public int IdPriloga { get => idPriloga; set => idPriloga = value; }
+        public int CenaJela { get => cenaJela; set => cenaJela = value; }
+        public int CenaPriloga { get => cenaPriloga; set => cenaPriloga = value; }
+    }
+}
